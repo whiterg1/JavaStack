70a216b57c647e5427de9226b8ef7c8c08db094f
Roy White <roy.g.white8@icloud.com>
2021-06-15 15:35:49 -0500

    Spring Projects

---
+++ SpringProjects/firstproject/src/main/java/com/codingdojo/firstproject/FirstprojectApplication.java
@@ -0,0 +1,23 @@
+package com.codingdojo.firstproject;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@SpringBootApplication
+@RestController
+public class FirstprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstprojectApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello World!";
+	}
+
+}
